3cfc6cd9d389d79ff68723bd51e1a35bb31bdf46
gorzki <rafal.rozniakowski@gmail.com>
2015-12-11 13:38:06 +0100

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get '/index' => 'home#index' 
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+
+end
