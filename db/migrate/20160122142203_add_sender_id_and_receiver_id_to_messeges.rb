bf4918fe89ac98069c1779e5e3c800f9c8a750e6
gorzki <rafal.rozniakowski@gmail.com>
2016-03-01 11:17:11 +0100

    Delete 20160122142203_add_sender_id_and_receiver_id_to_messeges.rb

--- db/migrate/20160122142203_add_sender_id_and_receiver_id_to_messeges.rb
+++
@@ -1,6 +0,0 @@
-class AddSenderIdAndReceiverIdToMesseges < ActiveRecord::Migration
-  def change
-    add_column :messeges, :sender_id, :integer
-    add_column :messeges, :receiver_id, :integer
-  end
-end
