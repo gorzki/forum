ac61cf25b2fc86e1784638cd415a45404f963e55
gorzki <rafal.rozniakowski@gmail.com>
2016-03-08 12:25:18 +0100

    all

--- spec/views/messages/show_spec.rb
+++ spec/views/messages/show_spec.rb
@@ -3,11 +3,12 @@ require "rails_helper"
 RSpec.describe "messages/show.html.haml", type: :view do
     before(:each) do
       @message = create(:message, :id => 1)
-      @messages = Array.new(2, create(:messages))
+      @messages = Array.new(3, create(:messages))
     end
     it 'can render' do
       render
-      assert_select "div", :text => "lolo".to_s, :count => 3
-      assert_select "div", :text => "polo".to_s, :count => 3
+
+      assert_select "div.panel-title", :text => "lolo".to_s, :count => 4
+      assert_select "div.panel-body", :text => "polo".to_s, :count => 4
     end
 end
